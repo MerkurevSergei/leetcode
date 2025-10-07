c5459f6baf1948b3ad30d5ea83dcd8e07c90219d
Merkurev Sergei <MerkurevSergei@yandex.ru>
2025-10-07 19:47:50 +0300

    [LEETCODE-11] Container With Most Water Solution
    Implemented two-pointer algorithm for Container With Most Water problem with test coverage.

---
+++ code/src/main/java/ru/merkurev/leetcode/problems/p0011/Solution.java
@@ -0,0 +1,54 @@
+package ru.merkurev.leetcode.problems.p0011;
+
+/**
+ * 11. Container With Most Water
+ * <p>
+ *     You are given an integer array height of length n. There are n vertical
+ *     lines drawn such that the two endpoints of the ith line are (i, 0)
+ *     and (i, height[i]).
+ * </p>
+ *
+ * <p>
+ *     Find two lines that together with the x-axis form a container, such that
+ *     the container contains the most water.
+ * </p>
+ *
+ * <p>
+ *     Return the maximum amount of water a container can store.
+ * </p>
+ *
+ * <p>
+ *     Notice that you may not slant the container.
+ * </p>
+ * <p>
+ *     Constraints:
+ * </p>
+ * <ul>
+ *     <li>n == height.length</li>
+ *     <li>2 <= n <= 105</li>
+ *     <li>0 <= height[i] <= 104</li>
+ * </ul>
+ */
+class Solution {
+
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+        
+        while (left < right) {
+            int width = right - left;
+            int currentHeight = Math.min(height[left], height[right]);
+            int currentArea = width * currentHeight;
+            maxArea = Math.max(maxArea, currentArea);
+            
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+}
